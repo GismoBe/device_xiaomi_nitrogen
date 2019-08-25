58e060164e7307bbe229f9d56e59fe08ecff93bc
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-08-25 14:47:15 +0200

    Update and rename aosip.mk to aosp_nitrogen.mk

--- aosp_nitrogen.mk
+++ aosp_nitrogen.mk
@@ -19,16 +19,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+#bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from nitrogen device
 $(call inherit-product, device/xiaomi/nitrogen/device.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-PRODUCT_NAME := aosip_nitrogen
+PRODUCT_NAME := aosp_nitrogen
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := nitrogen
 PRODUCT_MANUFACTURER := Xiaomi
