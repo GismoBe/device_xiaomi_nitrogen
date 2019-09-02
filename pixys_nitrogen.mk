8d2cbf7890b0d86bac44c0a2e72587dcb0bf4328
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-09-02 10:42:15 +0200

    Update and rename aosip.mk to pixys_nitrogen.mk

--- pixys_nitrogen.mk
+++ pixys_nitrogen.mk
@@ -23,14 +23,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/nitrogen/device.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-PRODUCT_NAME := aosip_nitrogen
+#Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+
+PRODUCT_NAME := pixys_nitrogen
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := nitrogen
+DEVICE_MAINTAINERS="GismoBe"\
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := MI MAX 3
 
